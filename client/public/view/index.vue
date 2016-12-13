65f5d98bb580a961f522c1612053d270d37596a6
jjjjyx <871839012@qq.com>
2016-12-13 23:29:15 +0800

    feat: 首页图片轮播

--- client/public/view/index.vue
+++ client/public/view/index.vue
@@ -3,30 +3,63 @@
 <template>
 <div class="container" id="main">
  <!-- 图片 -->
-    <div class="row img-paly">
-        <div class="col-md-8" >
-            图片轮播
+    <div class="row">
+        <div class="col-md-9" >
+            <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+                <!-- Indicators -->
+                <ol class="carousel-indicators">
+                    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+                    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+                    <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+                </ol>
+
+                <!-- Wrapper for slides -->
+                <div class="carousel-inner" role="listbox">
+                    <div class="item active">
+                      <img src="http://oht47c0d0.bkt.clouddn.com/16-12-13/92824295-file_1481639354189_9148.png" alt="...">
+                      <div class="acarousel-caption">
+                        ...asdasd
+                      </div>
+                    </div>
+                    <div class="item">
+                      <img src="http://oht47c0d0.bkt.clouddn.com/16-12-13/57533964-file_1481639376997_9cb7.png" alt="...">
+                      <div class="acarousel-caption">
+                          =====
+                      </div>
+                    </div>
+                    <div class="item">
+                      <img src="http://oht47c0d0.bkt.clouddn.com/16-12-13/97222073-file_1481639391407_16df4.png" alt="..." height="300">
+                      <div class="acarousel-caption">
+                          =====
+                      </div>
+                    </div>
+                </div>
+
+                <!-- Controls -->
+                <a class="left acarousel-control glyphicon glyphicon-chevron-left" href="#carousel-example-generic" role="button" data-slide="prev"></a>
+                <a class="right acarousel-control glyphicon glyphicon-chevron-right" href="#carousel-example-generic" role="button" data-slide="next"></a>
+            </div>
         </div>
-        <div class="col-md-4">
+        <div class="col-md-3">
             search or other
         </div>
     </div>
     <!-- 推广 -->
     <div class="row img-paly">
-        <div class="col-md-8" >
+        <div class="col-md-9" >
             --推广--
         </div>
-        <div class="col-md-4">
+        <div class="col-md-3">
 
         </div>
     </div>
 
     <!-- 最新文章 -->
     <div class="row img-paly">
-        <div class="col-md-8" >
+        <div class="col-md-9" >
             最新文章
         </div>
-        <div class="col-md-4">
+        <div class="col-md-3">
             最新评论
         </div>
     </div>
@@ -55,6 +88,7 @@ export default {
 
     },
     mounted: function () {
+        $('#carousel-example-generic').carousel()
     }
 }
 </script>
