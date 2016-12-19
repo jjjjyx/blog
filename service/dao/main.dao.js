74aacff5d6dcd74917fa63a7f9e8ca04a3e734df
jjjjyx <871839012@qq.com>
2016-12-19 23:00:33 +0800

    feat: 数据库链接，node需要6.0 以上版本

---
+++ service/dao/main.dao.js
@@ -0,0 +1,18 @@
+"use strict";
+
+import mysql from "mysql";
+
+let connection = mysql.createConnection({
+    host: '127.0.0.1',
+    user: 'blogjyx',
+    password: 'x.jyx.mysql'
+});
+
+connection.connect();
+
+connection.query('SELECT 1 + 1 AS solution',  (err, rows, fields) => {
+    if (err) throw err;
+    console.log('The solution is: ', rows[0].solution);
+});
+
+connection.end();
