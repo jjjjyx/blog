50030d9739546e61038d6cf4eb21cd65174b4c3f
jjjjyx <871839012@qq.com>
2016-12-23 00:16:47 +0800

    feat: 首页设计
    每天2小时😁~~
    
    git push

--- service/dao/user.dao.js
+++ service/dao/user.dao.js
@@ -1,19 +1,25 @@
-let db = require("./database");
+'use strict';
 
-db.pool.getConnection(function(err,connection){
-    let sql = "select * from tb_blog"
-    let addBlog = "insert into tb_blog(title,content,time,user_id,annexs,category) values(?,?,?,?,?,?)"
-    let val = [
-        'hello',
-        "### hello world",
-        Math.floor(+new Date()/1000),
-        0,
-        null,
-        0
-    ]
-    connection.query(addBlog,val,(err,result)=>{
-        console.log(err)
-        console.log(result);
-        connection.release();
-    })
-})
+let db = require("./database");
+// db.pool.getConnection(function(err,connection){
+//     let sql = "select * from tb_blog"
+//     let addBlog = "insert into tb_blog(title,content,time,user_id,annexs,category) values(?,?,?,?,?,?)"
+//     let val = [
+//         'hello',
+//         "### hello world",
+//         Math.floor(+new Date()/1000),
+//         0,
+//         null,
+//         0
+//     ]
+//     connection.query(addBlog,val,(err,result)=>{
+//         console.log(err)
+//         console.log(result);
+//         connection.release();
+//     })
+// })
+class User {
+    constructor () {
+
+    }
+}
--- service/test/test.es6
+++ service/test/test.es6
@@ -13,3 +13,20 @@ while (i++ < 1000000) {
 
 duration = +new Date() - start;
 console.log(duration);
+
+function a(time) {
+    return new Promise(function(){
+         setTimeout(function () {
+            resolve();
+        }, time);
+    })
+}
+
+var start = async function () {
+    // 在这里使用起来就像同步代码那样直观
+    console.log('start');
+    await sleep(3000);
+    console.log('end');
+};
+
+start();
