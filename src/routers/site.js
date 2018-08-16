2eaca71ed6343bd9292cda19da442914fc5d8077
jjjjyx <jyx@rpgame.net>
2018-08-16 23:37:50 +0800

    feat: 网站设置面板

--- src/routers/site.js
+++ src/routers/site.js
@@ -4,6 +4,7 @@ const express = require('express')
 
 const router = express.Router()
 const debug = require('debug')('app:routers:api.site')
+const log = require('log4js').getLogger('api.site')
 const utils = require('../utils')
 const Result = require('../common/resultUtils')
 const {siteDao, termDao} = require('../models');
@@ -11,41 +12,56 @@ const {body} = require('express-validator/check')
 const {sanitizeBody} = require('express-validator/filter')
 const {Enum, labels} = require('../common/enum')
 
-const update = [
-    sanitizeBody('key').trim(),
-    sanitizeBody('value').trim(),
-    body('key').exists().withMessage("key 为必须").isString().withMessage("请提交正确的key"),
-    body('value').exists().withMessage("value 为必须").isString().withMessage('请提及正确的value'),
-    utils.validationResult,
-    async function (req, res) {
-        let {key, value} = req.body
-        debug(`updateSite key= [${key}] value =[${value}]`)
-        try {
-            let values = {value}
-            await siteDao.update(values,{
-                where:{
-                    key
-                }
-            })
-            // 检查是否是在初始价值的值，是的话重新加载
+const updateSite = function(key, value) {
+    return siteDao.update({value}, {
+        where: {key}
+    })
+}
 
-            siteDao.findAll({
-                where: {
-                    autoLoad: Enum.SiteEnum.YES
-                }
-            }).then((sites)=>{
-            // 更新后 更新全局对象
-                debug('更新全局SITE')
-                // console.log('sites', sites)
-                let site = {}
-                sites.forEach((item)=>{
-                    site[item.key] = item.value
-                })
-                global.SITE = site
-                termDao.findById(SITE.defaultCategoryId).then(term => {
-                    global.SITE.defaultTerm = term
-                })
-            })
+const update = [
+    // sanitizeBody('key').trim(),
+    // sanitizeBody('value').trim(),
+    // body('key').exists().withMessage("key 为必须").isString().withMessage("请提交正确的key"),
+    // body('value').exists().withMessage("value 为必须").isString().withMessage('请提及正确的value'),
+    // utils.validationResult,
+    async function (req, res) {
+        req.sanitizeBody('keys').toArray()
+        req.sanitizeBody('values').toArray()
+        let {keys, values} = req.body
+        log.debug(`updateSite key= [%s] value =[%s]`, keys, values)
+        keys = keys || []
+        values = values || []
+
+        try {
+            let ups = []
+            if (keys.length !== values.length || keys.length === 0) {
+                return res.status(200).json(Result.info('参数个数不正确'))
+            }
+            // let values = {value}
+            // await siteDao.update(values,{
+            //     where:{
+            //         key
+            //     }
+            // })
+            // // 检查是否是在初始价值的值，是的话重新加载
+            //
+            // siteDao.findAll({
+            //     where: {
+            //         autoLoad: Enum.SiteEnum.YES
+            //     }
+            // }).then((sites)=>{
+            // // 更新后 更新全局对象
+            //     debug('更新全局SITE')
+            //     // console.log('sites', sites)
+            //     let site = {}
+            //     sites.forEach((item)=>{
+            //         site[item.key] = item.value
+            //     })
+            //     global.SITE = site
+            //     termDao.findById(SITE.defaultCategoryId).then(term => {
+            //         global.SITE.defaultTerm = term
+            //     })
+            // })
             return res.status(200).json(Result.success())
         } catch (e) {
             debug('editTerm error by :', e.message)
