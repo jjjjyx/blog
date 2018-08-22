ba8e6d89f5fbc3271fc6b577c63b8e7a54fc5807
jjjjyx <jyx@rpgame.net>
2018-08-22 11:46:14 +0800

    fix:icon 兼容iview

--- src/webapp/components/icon/font-icon.js
+++ src/webapp/components/icon/font-icon.js
@@ -54,6 +54,9 @@ export default {
             i = h('Icon', {
                 props: {
                     type, size, color, custom
+                },
+                on: {
+                    click: this.handleClick
                 }
             })
         }
